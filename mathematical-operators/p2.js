d563168e25ac029b9156b17d733bf528422b4858
PushpendraKuma <pushpendrakumar0484@gmail.com>
2022-09-20 12:48:55 +0530

    mathematical-operators

---
+++ mathematical-operators/p2.js
@@ -0,0 +1,7 @@
+let a = 2;
+let b = 3;
+let c = 4;
+let d = 5;
+let e = 6;
+sum = ((a*2)+(b*2)+(c*2)+(d*2)+(e*2))
+console.log(sum)
---
+++ mathematical-operators/p3.js
@@ -0,0 +1,8 @@
+let a = 3;
+let b = 4;
+let c = 5;
+let d = 6;
+let e = 7;
+let f = 8;
+sum = ((a)+(b*2)+(c*3)+(d*3)+(e*2)+(f*2))
+console.log(sum)
