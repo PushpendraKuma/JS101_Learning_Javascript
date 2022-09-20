e4396c9aa4e1f2f1b4a274eaaf0c50fcd4f6d304
PushpendraKuma <pushpendrakumar0484@gmail.com>
2022-09-20 12:03:09 +0530

    variables problems

---
+++ day1-variables/p2.js
@@ -0,0 +1,18 @@
+//Define a variable called name with your Name as the assigned value
+{
+  let name = "Pushpendra Kumar"
+father_name = "Jai Jai Ram"
+mother_name = "Rama Devi"
+console.log(name) 
+console.log(father_name)
+console.log(mother_name)
+
+
+
+
+
+
+
+
+  
+}
---
+++ day1-variables/p4.js
@@ -0,0 +1,31 @@
+//Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+
+{
+  let name = "Manoj"
+let school = "S.V.M"
+let grade = "A"
+let rollnumber = 12
+let section = "B"
+let subject1 = "physics"
+let subject2 = "chemistry"
+let subject3 = "math"
+let physics = 40
+let chemistry = 50
+let math = 60
+console.log(name)
+console.log(school)
+console.log(grade)
+console.log(rollnumber)
+console.log(section)
+console.log(subject1, subject2, subject3)
+console.log(physics, chemistry, math)
+
+  
+
+
+
+
+
+
+  
+}
